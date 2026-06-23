497c7a67d291bfd7eacbc45fbbbec00a527fb5b6
Prakash Chaurasia <prakashchaurasia959@gmail.com>
2026-06-23 11:07:12 +0530

    first commit

---
+++ Arrayproblems.js
@@ -0,0 +1,16 @@
+//Travers the array
+let Cricket=["virendra", " kapil dev","Rahul Dravid", " Lakshaman"," sidhu"]
+// for ( let i=0; i<=4;i++){
+//     console.log(Cricket[i]);
+// }
+// for(let i=0;i<=4;i++){
+//     if(Cricket[i]=="Rahul Dravid");
+
+// }
+//     //
+
+// Reverse Traversing
+for(let i=4;i>=0;i--){
+    console.log(Cricket[i]);
+    
+}
---
+++ Arrray.js
@@ -0,0 +1,49 @@
+
+// // Array create
+// let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+// console.log(fruits);
+
+// // length property
+// console.log("Length:", fruits.length);
+
+// // push() - last me add
+// fruits.push("Grapes");
+// console.log("push:", fruits);
+
+// // pop() - last element remove
+// fruits.pop();
+// console.log("pop:", fruits);
+
+// // unshift() - start me add
+// fruits.unshift("Pineapple");
+// console.log("unshift:", fruits);
+
+// // shift() - start se remove
+// fruits.shift();
+// console.log("shift:", fruits);
+
+// // indexOf() - position find
+// console.log("indexOf Banana:", fruits.indexOf("Banana"));
+
+// // includes() - element present hai ya nahi
+// console.log("includes Mango:", fruits.includes("Mango"));
+
+
+
+// //project2
+ let Num = [45,89,62,3,7,4]
+ Num.push(99)
+console.log(99);
+console.log(Num);
+Num.unshift(0)
+console.log(Num);
+Num.pop()
+
+
+
+
+
+
+
+
