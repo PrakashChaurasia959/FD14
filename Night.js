36a22430847282ead62bff65d687f79774cc2de1
Prakash Chaurasia <prakashchaurasia959@gmail.com>
2026-06-24 10:01:19 +0530

    kucch change kiya

---
+++ Night.js
@@ -0,0 +1,55 @@
+// let str = "I love babu";
+// str = str.replace("I", "S");
+
+// console.log(str);
+
+// let fullname =("enter your fullname without space");
+// let username ="@"+fullname+fullname.length;
+// console.log(username);
+// let marks = [97,82,82,45];
+// console.log(marks);
+// console.log(marks.length);
+ //let herores = 
+ //["ironman","super","hero","prakash","ram"];//
+// console.log(herores [0]);
+// console.log(herores[1])
+// for (let idx=0; idx<herores.length; idx++){
+//     console.log(herores[idx]);
+    
+// }
+// let marks = [87,97,67,56,90];
+
+// let sum = 0;
+// for (let val of marks){sum += val;}
+// let avg = sum / marks.length
+// console.log("avg marks of the class = ${avg}");
+
+
+// let items = [250,645,300,900,300,50];
+// let idx =0;
+// for(let val of items){
+//     console.log("value at index ${idx} = ${val}");
+//     idx++;
+    
+// }
+// let fooditems = ["y","j","p","c","d"];
+
+// console.log(fooditems);
+
+// let deleteditems = fooditems.pop();
+
+
+// console.log(fooditems);
+// console.log("deleted",deleteditems);
+// console.log(fooditems);
+// console.log(fooditems.toString);
+// let companies = ["Blooberg", "Microsoft","uber","google","ibm","met"]
+// console.log(companies);
+function myfunction(ram){
+    console.log(ram);}
+    myfunction("shyam");
+    
+
+
+
+
